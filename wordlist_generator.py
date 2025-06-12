cd18c77857af195ce240ba13afc5fef06f2059cc
theFivehead <marek.rohac5@gmail.com>
2025-06-12 14:32:19 +0200

    rewritten to use requests and BeautifulSoup library instead of Selenium making it faster

--- wordlist_generator.py
+++ wordlist_generator.py
@@ -1,84 +1,106 @@
 import multiprocessing
 import sys
 import time
-import argparse
+
 from math import ceil
-from multiprocessing import Process, cpu_count, Manager, Queue
+from multiprocessing import Process, cpu_count
 from os import cpu_count
-from selenium import webdriver
-from sys import argv
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.relative_locator import locate_with
-from selenium.webdriver.firefox.options import Options
+from urllib.parse import  urlparse
+
+import requests
+from bs4 import BeautifulSoup
+from urllib3.util import parse_url
+
+bsparser="html.parser"
+
+def extrahuj_slova(URL,fronta,verbose,pozice_dilu=1,pocet_dilu=1,maximum_stran=-1):
+    try:
+        prvniCastURL="https://"+parse_url(URL).netloc
+        seznam_strana = 0
+        predesly_nadpis = ""
+        kol_pro_zapsani=5
+        # nacitani a ukladani textu dokud nenajede na posledni stranku
+        while True:
+            if verbose:
+                print(f"PID:{multiprocessing.current_process().pid} - page:{seznam_strana}")
+            i = 0
+            #nacte si odkazy na jednotlive články
+            WikiSeznamHTML=BeautifulSoup(requests.get(URL).text,bsparser)
+            seznam_stranek=WikiSeznamHTML.find(attrs={"class":"mw-allpages-chunk"}).find_all("a")
+
+            text_buffer = []
+            omezeni_zhora=ceil(len(seznam_stranek) / pocet_dilu)
+            konec_rozsahu=(pozice_dilu+1) *  omezeni_zhora
+            zacatek_rozsahu=konec_rozsahu-omezeni_zhora
+            if konec_rozsahu > len(seznam_stranek):
+                konec_rozsahu=len(seznam_stranek)
+
+            stranky_URL = [""] * (konec_rozsahu-zacatek_rozsahu)
+
+            #extrahuje odkazy ze seznamu
+            j=0
+            for i in range(zacatek_rozsahu,konec_rozsahu):
+                stranky_URL[j] = prvniCastURL+seznam_stranek[i].get('href')
+                j+=1
+            #extrahuje text z jednotlivých stránek
+            for i in range(len(stranky_URL)):
+                #print(str(i) + ":" + stranky_URL[i]) #debugging
+                html=requests.get(stranky_URL[i]).text
+                html_parsed=BeautifulSoup(html,"html.parser")
+                nadpis=html_parsed.find(id='firstHeading').text
+                if predesly_nadpis == nadpis:
+                    continue
+                else:
+                    text_buffer.append(html_parsed.body.get_text().strip())
+                    predesly_nadpis=nadpis
 
 
+            #prejde na dalsi stranku se seznamem
+            URL_dalsi_stranka=WikiSeznamHTML.find(attrs={"class":"mw-allpages-nav"}).find_all("a")
+            print(URL_dalsi_stranka[-1].get('href'))
+            print(len(URL_dalsi_stranka))
+            print(len(URL_dalsi_stranka) <= 1 < seznam_strana)
+            print(seznam_strana >= maximum_stran != -1)
+            #zkontroluje jestli se nenachazi na posledni strance nebo neprekrocil limit stran
+            if (len(URL_dalsi_stranka) <= 1 < seznam_strana) or (seznam_strana >= maximum_stran != -1):
+                # postprocessing
+                for text in text_buffer:
+                    slova = text.split()
+                    fronta.put(list(set(slova)))
+                text_buffer.clear()
+                break
+            elif (seznam_strana+1)%kol_pro_zapsani==0: #zapise slova do spolecneho buferu
+                # postprocessing
+                for text in text_buffer:
+                    slova = text.split(text)
+                    fronta.put(list(set(slova)))
+                text_buffer.clear()
 
+            URL= prvniCastURL+URL_dalsi_stranka[-1].get('href')#prejde na dalsi stranku
+            print("URL: "+URL)
 
-def extrahuj_slova(URL,nastaveni,fronta,verbose,pozice_dilu=1,pocet_dilu=1,maximum_stran=-1):
-    driver=webdriver.Firefox(options=nastaveni)
-    driver.get(URL)
-    seznam_strana = 0
-    predesly_nadpis = ""
-    kol_pro_zapsani=5
-    # nacitani a ukladani textu dokud nenajede na posledni stranku
-    while True:
+            seznam_strana += 1
+    except KeyboardInterrupt:
         if verbose:
-            print(f"PID:{multiprocessing.current_process().pid} - page:{seznam_strana}")
-        i = 0
-        URL_seznam = driver.current_url
-        seznam_stranek = driver.find_element(By.CLASS_NAME, "mw-allpages-chunk").find_elements(By.TAG_NAME, "a")
-        text_buffer = []
-        omezeni_zhora=ceil(len(seznam_stranek) / pocet_dilu)
-        konec_rozsahu=(pozice_dilu+1) *  omezeni_zhora
-        zacatek_rozsahu=konec_rozsahu-omezeni_zhora
-        if konec_rozsahu > len(seznam_stranek):
-            konec_rozsahu=len(seznam_stranek)
-
-        stranky_URL = [""] * (konec_rozsahu-zacatek_rozsahu)
-        #extrahuje odkazy ze seznamu
-        j=0
-        for i in range(zacatek_rozsahu,konec_rozsahu):
-            stranky_URL[j] = seznam_stranek[i].get_attribute("href")
-            j+=1
-        #extrahuje text z jednotlivých stránek
-        for i in range(len(stranky_URL)):
-            #print(str(i) + ":" + stranky_URL[i])
-            driver.get(stranky_URL[i])
-            nadpis=driver.find_element(By.ID,"firstHeading").text
-            if predesly_nadpis == nadpis:
-                continue
-            else:
-                text_buffer.append(driver.find_element(By.ID, "bodyContent").text)
-                predesly_nadpis=nadpis
-        driver.get(URL_seznam)
-
-        #prejde na dalsi stranku se seznamem
-        dalsi_stranka = driver.find_element(By.CLASS_NAME, "mw-allpages-nav").find_elements(By.TAG_NAME, "a")
-        dalsi_stranka[-1].click()
-        if len(driver.find_element(By.CLASS_NAME, "mw-allpages-nav").find_elements(By.TAG_NAME, "a"))<=1 or (seznam_strana >= maximum_stran !=-1):
-            # postprocessing
-            for text in text_buffer:
-                slova = text.split()
-                fronta.put(list(set(slova)))
-            text_buffer.clear()
-            break
-        elif (seznam_strana+1)%kol_pro_zapsani==0: #zapise slova do spolecneho buferu
-            # postprocessing
-            for text in text_buffer:
-                slova = text.split(text)
-                fronta.put(list(set(slova)))
-            text_buffer.clear()
-
-        seznam_strana += 1
+            print(f"ending PID:{multiprocessing.current_process().pid}")
+        for text in text_buffer:
+            slova = text.split()
+            fronta.put(list(set(slova)))
+        text_buffer.clear()
+        return 1
     #radne ukonceni procesu
-    driver.quit()
 
 
 
 def cteni_queue(ukonceni,fronta,hlavni_buffer):
+
     while not ukonceni.value:
-        while not fronta.empty():
-            hlavni_buffer.extend(fronta.get())
+        try:
+            while not fronta.empty():
+                hlavni_buffer.extend(fronta.get())
+        except KeyboardInterrupt:
+            print("stop")
+            ukonceni.value=1
     unikatni_slova=list(set(hlavni_buffer))
     hlavni_buffer[:]=unikatni_slova
 
@@ -89,6 +111,8 @@ def main():
     help=False
     ukecany=False
     headless=True
+    pocet_CPU = cpu_count()
+    dily = pocet_CPU * 2  # vypocte kolik prohlizecu bude bezet zaroven
     wp_kod=sys.argv[1]
     max_stran=-1
     if wp_kod.find("-") == 0:
@@ -108,12 +132,9 @@ def main():
             elif flag.find("-")!=-1: #kratke argumenty
                 if flag.find("v")!=-1:
                     ukecany=True
-                if flag.find("g")!=-1:
-                    headless=False
     if help:
         print("""wordlist_generator.py [WP] -možnosti
         -v - ukecaný
-        -g - grafický - vypne headless mode prohlížeče a zobrazí je na obrazovku
         --processes=N - počet procesů - nastaví kolik prohlížečů pojede zároveň. V základu běží dvojnásobek počtu CPU jader
         --limit=N - maximální počet stran - do kolika stran wikipedie program pojede (pokud není nastavený limit, vezme všechny)
         --help - zobrazí tuto stránku
@@ -122,51 +143,66 @@ def main():
         """)
         return 0
 
-    #nastavi driver
-    nastaveni=Options()
-    nastaveni.add_argument("--width=1024")
-    nastaveni.add_argument("--height=720")
-    if headless:
-        nastaveni.add_argument("--headless")
-    driver = webdriver.Firefox(options=nastaveni)
 
-    # nacte list wikipedia stranek
-    driver.get("https://en.wikipedia.org/wiki/List_of_Wikipedias")
-    jazyk = driver.find_element(By.XPATH, "//*[text()='" + wp_kod + "']")
-    url = driver.find_element(locate_with(By.TAG_NAME, "a").straight_right_of(jazyk))
 
-    driver.get(url.get_attribute("href"))
-    # presmeruje se na URL se seznamem stranek
-    driver.get(
-        driver.find_element(By.CLASS_NAME, "mw-statistics-pages").find_element(By.TAG_NAME, "a").get_attribute("href"))
-    time.sleep(1)
-    zacatecni_URL = driver.current_url
-    driver.quit()
+    # nacte list wikipedia stranek a extrahuje URL pro dany WP kod
+    print(wp_kod)
+    jazyk = BeautifulSoup(requests.get("https://en.wikipedia.org/wiki/List_of_Wikipedias").text,bsparser).find("a",string=wp_kod).parent.parent.next_sibling()
+    print(jazyk)
+
+    if jazyk is None:
+        print("code "+wp_kod+" doesnt exist")
+        sys.exit()
+    url=jazyk[0].get('href')
+    print(url)
+    if url.find("https:") == -1:
+        print("code "+wp_kod+" doesnt exist")
+        sys.exit()
+
+    #driver.get(url.get_attribute("href"))
+    requests.get(url)
+    # presmeruje se na URL se seznamem clanku
+    r=requests.get(url)
+    seznam_clanku=BeautifulSoup(r.text,bsparser).find(attrs={"class":"mw-statistics-pages"}).find("a",href=True).get('href')
+    print(seznam_clanku)
+
+    zacatecni_URL="https://"+urlparse(url).netloc+seznam_clanku #prvni URL se seznamem
+    print(zacatecni_URL)
+
+
     if ukecany:
         print(f"first page URL:{zacatecni_URL}")
     #inicializace multiprocessingu ---------------
     fronta = multiprocessing.Queue()  # zde se slevaji vsechny stringy s procesu ze, ktereho se nakonce vytvori wordlist
     hlavni_buffer=multiprocessing.Manager().list()
-    pocet_CPU=cpu_count()
+
     ukonceni=multiprocessing.Manager().Value("i",0)
-    dily=pocet_CPU*2 #vypocte kolik prohlizecu bude bezet zaroven
+
+
     if ukecany:
-        print(f"number of browsers to open:{dily}")
+        print(f"number of processes to start:{dily}")
     procesy=[""]*dily
     zpracovani_fronty=Process(target=cteni_queue, args=(ukonceni, fronta,hlavni_buffer))
-    for i in range(len(procesy)):
-        procesy[i]=Process(target=extrahuj_slova, args=(zacatecni_URL,nastaveni ,fronta,ukecany ,i, dily,max_stran))
-    for i in range(len(procesy)):
-        procesy[i].start()
-        if ukecany:
-            print(f"Process {i+1} started with PID:{procesy[i].pid}")
-    zpracovani_fronty.start()
-    print("world list processing has started")
-    for i in range(len(procesy)):
-        procesy[i].join()
-
-    ukonceni.value=1
-    time.sleep(0.5)
+    try:
+        #vytvori a spusti procesy
+        for i in range(len(procesy)):
+            procesy[i]=Process(target=extrahuj_slova, args=(zacatecni_URL,nastaveni ,fronta,ukecany ,i, dily,max_stran))
+        for i in range(len(procesy)):
+            procesy[i].start()
+            if ukecany:
+                print(f"Process {i+1} started with PID:{procesy[i].pid}")
+        zpracovani_fronty.start()
+        print("world list processing has started")
+        for i in range(len(procesy)):
+            procesy[i].join()
+        ukonceni.value = 1
+        time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("ending script please wait")
+        ukonceni.value = 1
+        time.sleep(0.5)
+        for i in range(len(procesy)):
+            procesy[i].join()
     if zpracovani_fronty.is_alive():
         time.sleep(2)
         zpracovani_fronty.terminate()
